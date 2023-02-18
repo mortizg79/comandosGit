d9bf60727619a60d8f3faff7e87f5d8c1f6e4ee1
Mario Ortiz Gómez <mortizg79@hotmail.com>
2023-02-17 23:23:40 -0600

    Watherforcars

--- ComandosGitWebAPI/WeatherForecast.cs
+++ ComandosGitWebAPI/WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace ComandosGitWebAPI
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+        public string Descripcion { get; set; }
+        public bool IsActive { get; set; }
     }
 }
