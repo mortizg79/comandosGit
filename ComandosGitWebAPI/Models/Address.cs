d7edb03f921bd487fb0241d7fb034ae3e207e7c9
Mario Ortiz Gómez <mortizg79@hotmail.com>
2023-02-17 23:30:38 -0600

    Entidad Direccion

---
+++ ComandosGitWebAPI/Models/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace ComandosGitWebAPI.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string AddressDescription { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
