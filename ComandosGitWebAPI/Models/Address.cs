803dcc26f9e3b42d62cf589a005c8aa70cb0992e
Mario Ortiz Gómez <mortizg79@hotmail.com>
2023-02-17 23:40:54 -0600

    properties Addres Entities

--- ComandosGitWebAPI/Models/Address.cs
+++ ComandosGitWebAPI/Models/Address.cs
@@ -4,6 +4,10 @@
     {
         public int Id { get; set; }
         public string AddressDescription { get; set; }
+        public int PostalCode { get; set; }
+        public int NumberExt { get; set; }
+        public int NumberInt { get; set; }
         public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
     }
 }
