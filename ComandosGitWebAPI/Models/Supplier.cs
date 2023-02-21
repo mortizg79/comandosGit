1876f18a92a3e389b237c8b4e0e5dc9010185235
Mario Ortiz Gómez <mortizg79@hotmail.com>
2023-02-21 13:42:55 -0600

    Supplier Entitie

---
+++ ComandosGitWebAPI/Models/Supplier.cs
@@ -0,0 +1,11 @@
+﻿namespace ComandosGitWebAPI.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PostalCode { get; set; }
+        public bool IsActive { get; set; }
+        public decimal Price { get; set; }
+    }
+}
